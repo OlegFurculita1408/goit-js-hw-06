cccfb225eb7fa741e1cccdf1830a9ba880956847
Oleg Furculita <Oleg.Furculita1408@gmail.com>
2023-04-18 22:01:06 +0300

    finish task 2

---
+++ js/task-01.js
@@ -0,0 +1,14 @@
+//  1.1. Посчитает и выведет в консоль количество категорий в ul#categories, то есть элементов li.item.
+
+const liItemAll = document.querySelectorAll('.item')
+console.log(`Number of categories: ${liItemAll.length}`)
+
+//  1.2. Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка
+//      элемента (тега <h2>) и количество элементов в категории (всех вложенных в него <li>).
+
+liItemAll.forEach(e => {
+    console.log(`Category: ${e.firstElementChild.textContent}`)
+    console.log(`Elements: ${e.lastElementChild.querySelectorAll('li').length}`)
+});
+
+
--- js/task-02.js
+++ js/task-02.js
@@ -1,3 +1,9 @@
+// 2 Напиши скрипт, который для каждого элемента массива ingredients:
+// Создаст отдельный элемент <li>. Обзательно используй метод document.createElement().
+// Добавит название ингредиента как его текстовое содержимое.
+// Добавит элементу класс item.
+// После чего вставит все <li> за одну операцию в список ul#ingredients.
+
 const ingredients = [
   'Potatoes',
   'Mushrooms',
@@ -6,3 +12,8 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+ingredients.forEach((item) => {
+  let list = document.createElement('li');
+  list.innerText = item;
+  document.querySelector('#ingredients').append(list);
+});

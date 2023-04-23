1582ff92122502c99f44557a9ca5d4302c43cc86
Oleg Furculita <Oleg.Furculita1408@gmail.com>
2023-04-23 15:21:59 +0300

    finish 9 task

--- js/task-08.js
+++ js/task-08.js
@@ -4,3 +4,40 @@
 // Если пользователь заполнил все поля и отправил форму, собери значения полей в обьект, где имя поля будет именем свойства,
 //      а значение поля - значением свойства. Для доступа к элементам формы используй свойство elements.
 // Выведи обьект с введенными данными в консоль и очисти значения полей формы методом reset.
+
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+const formElements = event.currentTarget.elements;
+
+const mail = formElements.email.value;
+const password = formElements.password.value;
+
+    if (mail === '' || password === '') {
+        alert('All fields must be filled !');
+    }
+    else {
+        // 1 =====
+        // console.log(new Object(`mail: ${mail}, password: ${password}`));
+
+        // 2 =====
+        const formData = {
+            mail,
+            password,
+        };
+        console.log(formData);
+
+        // 3 =====
+        // const formData = new FormData(event.currentTarget);
+        // formData.forEach((value, name) => {
+        //     const obj = new Object(`${name}: ${value}`);
+        //     console.log(obj);
+        // });
+        
+    }
+    form.reset();
+};
+
--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,15 @@
+// Напиши скрипт, который изменяет цвета фона элемента <body>
+//  через инлайн стиль при клике на button.change-color и выводит значение цвета в span.color.
+
+const span = document.querySelector('.color');
+const btnColor = document.querySelector('.change-color');
+const body = document.querySelector('body')
+
+btnColor.addEventListener('click', getRandomHexColor);
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
+  const colorBody = body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-}
+    span.textContent = colorBody;
+};
